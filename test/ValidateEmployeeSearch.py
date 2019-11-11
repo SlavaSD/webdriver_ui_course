482cea6c0c8b47c7d7bdb5164081ddf75f948458
admin <admin@admins-MacBook-Pro.local>
2019-11-10 17:21:33 -0800

    Employee search added

--- test/ValidateEmployeeSearch.py
+++ test/ValidateEmployeeSearch.py
@@ -2,6 +2,10 @@ import unittest
 from selenium import webdriver
 from time import sleep
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 class ValidEmpSrch(unittest.TestCase):
 
@@ -9,8 +13,9 @@ class ValidEmpSrch(unittest.TestCase):
         self.driver = webdriver.Chrome(executable_path='/Users/admin/Desktop/HRM_Summer2019/browser_drivers/chromedriver')
         self.driver.get('http://hrm-online.portnov.com/')
 
-    def tearDown(drv):
-        drv.driver.quit()
+
+    def tearDown(self):
+        self.driver.quit()
 
 # 1) Go to hrm.seleniumminutes.com
 # 2) Login: admin/Password
@@ -20,21 +25,21 @@ class ValidEmpSrch(unittest.TestCase):
 # 6) Check that the first row of the results table contains an employee with last name 'Smith'
 # Hint: Don't forget to wait where appropriate!
 
-    def test_something(drv):
-        drv = drv.driver
-        drv.find_element_by_id('txtUsername').send_keys('admin')
-        drv.find_element_by_id('txtPassword').send_keys('password')
-        drv.find_element_by_id('btnLogin').click()
-        drv.find_element_by_id('menu_pim_viewPimModule').click()
-        drv.find_element_by_id('empsearch_employee_name_empName').send_keys('Smith')
-        drv.find_element_by_id('searchBtn').click()
+    def test_something(self):
+        driver = self.driver
+        self.wait = WebDriverWait(self.driver, 2)
+        driver.find_element_by_id('txtUsername').send_keys('admin')
+        driver.find_element_by_id('txtPassword').send_keys('password')
+        driver.find_element_by_id('btnLogin').click()
+        #driver.find_element_by_id('menu_pim_viewPimModule').click()
+        #sleep(0.5)
+        driver.find_element_by_id('empsearch_employee_name_empName').send_keys('Smith')
+        #self.wait.until(expected_conditions.presence_of_element_located((By.ID, 'empsearch_employee_name_empName'))).send_keys('Smith')
+        #self.wait.until(expected_conditions.presence_of_element_located((By.ID, 'searchBtn'))).click()
+        driver.find_element_by_id('searchBtn').click()
+        result = self.wait.until(expected_conditions.presence_of_element_located((By.XPATH, '//tbody/tr/td[4]'))).text
 
-
-
-
-
-
-        drv.assertEqual(True, False)
+        self.assertEqual("Smith", result)
 
 
 
